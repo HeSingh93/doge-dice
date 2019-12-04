7347fd745e09d723b4615d48356681c35fe39e28
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-12-04 13:24:51 +0100

    added mockito to dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 }
 
 dependencies {
+  testCompile("org.mockito:mockito-core:2.+")
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
   compile("org.slf4j:slf4j-simple:1.7.21")
