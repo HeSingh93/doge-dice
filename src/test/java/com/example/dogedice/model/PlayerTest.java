8258d28eb8a40a6f9646408cb7a490c86eddb9f4
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2019-12-06 19:53:49 +0100

    Testmethod for adding twentysideddie and refactor method names

--- src/test/java/com/example/dogedice/model/PlayerTest.java
+++ src/test/java/com/example/dogedice/model/PlayerTest.java
@@ -14,21 +14,28 @@ class PlayerTest {
   }
 
   @Test
-  void addModifier_AddValueOfOne_PlayerAddModifier() {
+  void addModifier_ModifierListHasSizeOne_AfterAddingOneModifier() {
     player.addModifier(new Modifier(1));
     assertEquals(1, player.getModifiers().size());
   }
+
   @Test
-  void removePoints_failToRemovePoints() {
+  void removePoints_returnsFalse_IfPlayerHasInsufficientPoints() {
     boolean remove = player.removePoints(1);
     assertFalse(remove, "Failed to remove Points");
   }
 
   @Test
-  void addDie_addTenDiceToPlayer() {
+  void addDie_DiceListHasSizeTen_AfterAddingTenDice() {
     for (int i = 1; i <= 10; i++) {
       player.addDie(new Die(6));
     }
     assertEquals(10, player.getDice().size());
+  }
+
+  @Test
+  void addDie_DiceListHasTwentySidedDie_AfterAddingTwentySidedDie() {
+    player.addDie(new Die(20));
+    assertEquals(20, player.getDice().get(0).getNumOfSides());
   }
 }
